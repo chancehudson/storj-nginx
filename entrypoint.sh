4b055a641fc670b602551bc16255e64f07c8ac7c
Chance Hudson <jchancehud@gmail.com>
2018-02-02 02:56:02 -0600

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,27 +6,14 @@
 SECRETS=/run/secrets/
 
 # Load libstorj env config options if they are present
-if [ -f $SECRETS/STORJ_KEYPASS ]; then
-  export STORJ_KEYPASS=$(cat $SECRETS/STORJ_KEYPASS)
-fi
-
-if [ -f $SECRETS/STORJ_BRIDGE ]; then
-  export STORJ_BRIDGE=$(cat $SECRETS/STORJ_BRIDGE) # This defaults to https://api.storj.io
-fi
-
-if [ -f $SECRETS/STORJ_BRIDGE_USER ]; then
-  export STORJ_BRIDGE_USER=$(cat $SECRETS/STORJ_BRIDGE_USER)
-fi
-
-if [ -f $SECRETS/STORJ_BRIDGE_PASS ]; then
-  export STORJ_BRIDGE_PASS=$(cat $SECRETS/STORJ_BRIDGE_PASS)
-fi
-
-if [ -f $SECRETS/STORJ_ENCRYPTION_KEY ]; then
-  export STORJ_ENCRYPTION_KEY=$(cat $SECRETS/STORJ_ENCRYPTION_KEY)
-fi
+[ -f $SECRETS/STORJ_KEYPASS ] && STORJ_KEYPASS=$(cat $SECRETS/STORJ_KEYPASS)
+[ -f $SECRETS/STORJ_BRIDGE ] && STORJ_BRIDGE=$(cat $SECRETS/STORJ_BRIDGE) # This defaults to https://api.storj.io
+[ -f $SECRETS/STORJ_BRIDGE_USER ] && STORJ_BRIDGE_USER=$(cat $SECRETS/STORJ_BRIDGE_USER)
+[ -f $SECRETS/STORJ_BRIDGE_PASS ] && STORJ_BRIDGE_PASS=$(cat $SECRETS/STORJ_BRIDGE_PASS)
+[ -f $SECRETS/STORJ_ENCRYPTION_KEY ] && STORJ_ENCRYPTION_KEY=$(cat $SECRETS/STORJ_ENCRYPTION_KEY)
 
 ls -la /run/secrets
+cat $SECRETS/STORJ_BRIDGE_USER
 rm /etc/nginx/nginx.conf
 storj download-file $CONFIG_BUCKET_ID $CONFIG_FILE_ID /etc/nginx/nginx.conf
 
